325f5af9c4bf7d4ab057373a4a9009103d109b3a
ashcodes101010 <ashleighcoltman@college.harvard.edu>
2021-01-13 12:16:31 -0500

    Day 8 AM

--- src/graphql/typeDefs.js
+++ src/graphql/typeDefs.js
@@ -3,5 +3,70 @@ const { gql } = require('apollo-server-express')
 module.exports = gql`
   type Query {
     welcome: String!
+    authors: [Author!]!
+    books: [Book!]!
+    publishers: [Publisher!]!
+    authorById(id: ID!): Author!
+    bookById(id: ID!): Book!
+  }
+  type Mutation {
+    addAuthor(input: addAuthorInput): Author!
+    addBook(input: addBookInput): Book!
+    addPublisher(input: addPublisherInput): Publisher!
+  }
+  input addAuthorInput {
+    firstName: String!
+    lastName: String!
+    age: Int!
+    email: String!
+    address: Address!
+  }
+  input addPublisherInput {
+    company: String!
+    phoneNumber: String!
+    numBooksPublished: Int!
+    address: Address!
+  }
+  input addBookInput {
+    title: String!
+    language: String
+    numPages: Int
+    datePublished: String!
+    bestseller: Boolean! 
+    author: Author!
+    publisher: Publisher!
+  }
+  type Author {
+    id: ID!
+    firstName: String!
+    lastName: String!
+    age: Int!
+    email: String!
+    address: Address!
+    books: [Book!]!
+  }
+  type Address {
+    id: ID!
+    street: String!
+    city: String!
+    state: String
+    zip: String
+  }
+  type Book {
+    id: ID!
+    title: String!
+    language: String
+    numPages: Int
+    datePublished: String!
+    bestseller: Boolean! 
+    author: Author!
+    publisher: Publisher!
+  }
+  type Publisher {
+    id: ID!
+    company: String!
+    phoneNumber: String!
+    numBooksPublished: Int!
+    address: Address!
   }
 `
